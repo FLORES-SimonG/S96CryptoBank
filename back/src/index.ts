c83aa15fc23b7f660b35839c291853cb0b00525b
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-02-27 23:32:25 +0100

    adelanto de la HM Express-TS
    A partir de acá estaré comenzando con creación de rutas. Las dependencias ya las instalé.

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hola mundo! Soy simon')
+const express = require('express');
+require('dotenv').config();
+
+const PORT=process.env.PORT || 3000;
+
+const server = express();
+
+server.listen(PORT, () => {
+    console.log(`El server está escuchando en el puerto n° ${PORT}`);
+});
