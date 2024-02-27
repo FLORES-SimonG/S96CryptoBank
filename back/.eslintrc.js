f2a5731f01f3c5f30485bb0bb07ee7bafdb2103a
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-02-27 21:21:32 +0100

    Configuración de Entorno
    Realizada la configuración de entorno.

---
+++ back/.eslintrc.js
@@ -0,0 +1,6 @@
+// module.exports = {
+//     parser: '@typescript-eslint/parser',
+//     pulgins: ['@typescript-eslint'],
+//     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+//     rules: {
+//     },};
---
+++ front/index.ts
@@ -0,0 +1,7 @@
+const sumar=(arg1:string,arg2:number)=>{
+    console.log(arg1,arg2);
+    let suma:number=Number(arg1)+arg2;
+    return suma;
+};
+
+sumar('84',5);
