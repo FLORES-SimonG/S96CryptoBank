c1b659bdceee94065aa69463c53fedf8b902fecc
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-03-06 01:10:58 +0100

    React Intro: completo
    Solamente implemente un estilo para comprobar el funcionamiento correcto.

---
+++ front/react-intro/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+
+const Home=()=>{
+    return(
+        <>
+        <h1>componente home</h1>
+        <Navbar/>
+        </>
+    )
+    }
+    
+    export default Home;
---
+++ front/react-intro/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <>
+        <nav style={{backgroundColor:'red'}}>
+        <p >Este es un nav</p>
+        
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/create">ESTE ES UN NAV</a></li>
+            </ul>
+            
+        
+        </nav>
+        </>
+    );
+    }
+
+    export default Navbar;
