d76df16f665931092abfd3530cce283b7f2c005a
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-03-10 12:58:01 +0100

    Funcionalidad de Register hecha
    Ahora tanto Register como Login debo conectarlas a la BBDD

--- front/react-intro/src/view/Login/Login.jsx
+++ front/react-intro/src/view/Login/Login.jsx
@@ -1,86 +1,83 @@
-import { useState } from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import styles from './Login.module.css';
-import axios from 'axios';
-
-
+import { useState } from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./Login.module.css";
+import axios from "axios";
 
 const Login = () => {
-    const [itemsFromLogin,setItemsFromLogin] = useState({
-        username: '',
-        password: ''
+  const [itemsFromLogin, setItemsFromLogin] = useState({
+    username: "",
+    password: "",
+  });
+
+  console.log(itemsFromLogin);
+
+  //* Funcion MANEJADORA el cambio de los inputs de LOGIN
+  const handlerInputChangeFromLogin = (evento) => {
+    // console.log(evento);
+
+    //?OPCION 1:
+    const { name, value } = evento.target;
+    setItemsFromLogin({
+      ...itemsFromLogin,
+      [name]: value,
     });
 
-    console.log(itemsFromLogin);
- 
-    //* Funcion MANEJADORA el cambio de los inputs
-    const handlerInputChange= (evento)=>{
-        
-        // console.log(evento);
+    //?OPCION 2:
+    // const {name,value} = evento.target;
+    // if (name === 'username') {
+    //     setItemsFromLogin({
+    //         ...itemsFromLogin,
+    //         username: value
+    //     })
+    // }
 
-        //?OPCION 1:
-        // const {name,value} = evento.target;
-        // if (name === 'username') {
-        //     setItemsFromLogin({
-        //         ...itemsFromLogin,
-        //         username: value
-        //     })
-        // }
+    // if (name === 'password') {
+    //     setItemsFromLogin({
+    //         ...itemsFromLogin,
+    //         password: value
+    //     })
+    // }
+  };
 
-        // if (name === 'password') {
-        //     setItemsFromLogin({
-        //         ...itemsFromLogin,
-        //         password: value
-        //     })
-        // }
+  //* Funcion MANEJADORA para el submit del formulario de LOGIN
+  const handleOnSubmitFromLogin = (evento) => {
+    evento.preventDefault();
+    alert("Formulario de LOGIN enviado");
+  };
 
-        //?OPCION 2:
-        const {name,value} = evento.target;
-        setItemsFromLogin({
-            ...itemsFromLogin,
-            [name]: value
-        })
-        
-    }
-
-    //* Funcion MANEJADORA para el submit del formulario
-    const handleOnSubmit = (evento)=>{
-        evento.preventDefault();
-        console.log('Formulario enviado');
-        }  
-
-
-
-
-
-
-
-
-    return(
-        <div>
-         <Navbar></Navbar>
-            <div className={styles.login}>
-                            <div class={styles.formContainer}>
-                                <p class={styles.title}>Login</p>
-                                <form class={styles.form} onSubmit={handleOnSubmit}>
-                                    <div class={styles.inputGroup}>
-                                        <label >Username</label>
-                                        <input type="text" value={itemsFromLogin.username}
-                                        name='username'
-                                        placeholder='' onChange={handlerInputChange}></input>  
-                                       
-                                    </div>
-                                    <div class={styles.inputGroup}>
-                                        <label >Password</label>
-                                        <input type="password" name="password" value={itemsFromLogin.password} placeholder="" onChange={handlerInputChange}></input>  
-                                    </div>
-                                    <button class={styles.sign}>Sign in</button>
-                                </form>
-                            </div>
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className={styles.login}>
+        <div class={styles.formContainer}>
+          <p class={styles.title}>Login</p>
+          <form class={styles.form} onSubmit={handleOnSubmitFromLogin}>
+            <div class={styles.inputGroup}>
+              <label>Username</label>
+              <input
+                type="text"
+                value={itemsFromLogin.username}
+                name="username"
+                placeholder=""
+                onChange={handlerInputChangeFromLogin}
+              ></input>
             </div>
+            <div class={styles.inputGroup}>
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                value={itemsFromLogin.password}
+                placeholder=""
+                onChange={handlerInputChangeFromLogin}
+              ></input>
+            </div>
+            <button class={styles.sign}>Sign in</button>
+          </form>
         </div>
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login;;
+export default Login;
--- front/react-intro/src/view/Register/Register.jsx
+++ front/react-intro/src/view/Register/Register.jsx
@@ -1,48 +1,151 @@
-import Navbar from '../../components/Navbar/Navbar';
-import styles from './Register.module.css';
-
+import Navbar from "../../components/Navbar/Navbar";
+import styles from "./Register.module.css";
+import axios from "axios";
+import { useState } from "react";
 
 const Register = () => {
-    const today = new Date().toISOString().split('T')[0];
-    return(
-        <div>
-            
-         <Navbar></Navbar>
-            <div className={styles.register}>
-                            <div class={styles.formContainer}>
-                                <p class={styles.title}>Register</p>
-                                <form class={styles.form}>
-                                    <div class={styles.inputGroup}>
-                                        <label for="name">Name</label>
-                                        <input type="text" name="name" id="name" placeholder=""></input>
-                                    </div>
-                                    <div class={styles.inputGroup}>
-                                        <label for="email">Email</label>
-                                        <input type="text" name="email" id="email" placeholder=""></input>
-                                    </div>
-                                    <div class={styles.inputGroup}>
-                                        <label htmlFor="birthdate">Birthdate</label>
-                                        <input type="date" name="birthdate" id="birthdate" max={today} placeholder=""/>
-                                    </div>
-                                    <div class={styles.inputGroup}>
-                                        <label for="nDni">ID-CardNumber</label>
-                                        <input type="number" name="nDni" id="nDni" placeholder=""></input>
-                                    </div>
-                                    <div class={styles.inputGroup}>
-                                        <label for="username">Username</label>
-                                        <input type="text" name="username" id="username" placeholder=""></input>
-                                    </div>
-                                    <div class={styles.inputGroup}>
-                                        <label for="password">Password</label>
-                                        <input type="password" name="password" id="password" placeholder=""></input>  
-                                    </div>
-                                    <button class={styles.sign}>Sign in</button>
-                                </form>
-                            </div>
-            </div>
-        </div>
-        
-    )
-}
+  const today = new Date().toISOString().split("T")[0];
+  const [itemsFromRegister, setItemsFromRegister] = useState({
+    name: "",
+    email: "",
+    birthdate: "",
+    nDni: "",
+    username: "",
+    password: "",
+  });
+  console.log(itemsFromRegister);
 
-export default Register;;
+  //* Funcion MANEJADORA en el cambio de los inputs de REGISTER
+
+  //? OPCION 1:
+  const handlerInputChangeFromRegister = (evento) => {
+    const { name, value } = evento.target;
+    setItemsFromRegister({
+      ...itemsFromRegister,
+      [name]: value,
+    });
+
+    //? OPCION 2:
+    //const {name,value} = evento.target;
+    // if (name === 'name') {
+    //     setItemsFromRegister({
+    //         ...itemsFromRegister,
+    //         name: value
+    //     })
+    // }
+    // if (name === 'email') {
+    //     setItemsFromRegister({
+    //         ...itemsFromRegister,
+    //         email: value
+    //     })
+    // }
+    // if (name === 'birthdate') {
+    //     setItemsFromRegister({
+    //         ...itemsFromRegister,
+    //         birthdate: value
+    //     })
+    // }
+    // if (name === 'nDni') {
+    //     setItemsFromRegister({
+    //         ...itemsFromRegister,
+    //         nDni: value
+    //     })
+    // }
+    // if (name === 'username') {
+    //     setItemsFromRegister({
+    //         ...itemsFromRegister,
+    //         username: value
+    //     })
+    // }
+    // if (name === 'password') {
+    //     setItemsFromRegister({
+    //         ...itemsFromRegister,
+    //         password: value
+    //     })
+    // }
+  };
+
+  //* Funcion MANEJADORA para el submit del formulario de REGISTER
+
+  const handleOnSubmitFromRegister = (evento) => {
+    evento.preventDefault();
+    alert("Formulario de REGISTRO enviado");
+  };
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className={styles.register}>
+        <div class={styles.formContainer}>
+          <p class={styles.title}>Register</p>
+          <form class={styles.form} onSubmit={handleOnSubmitFromRegister}>
+            <div class={styles.inputGroup}>
+              <label for="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder=""
+                onChange={handlerInputChangeFromRegister}
+              ></input>
+            </div>
+            <div class={styles.inputGroup}>
+              <label for="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder=""
+                onChange={handlerInputChangeFromRegister}
+              ></input>
+            </div>
+            <div class={styles.inputGroup}>
+              <label htmlFor="birthdate">Birthdate</label>
+              <input
+                type="date"
+                name="birthdate"
+                id="birthdate"
+                max={today}
+                placeholder=""
+                onChange={handlerInputChangeFromRegister}
+              />
+            </div>
+            <div class={styles.inputGroup}>
+              <label for="nDni">ID-CardNumber</label>
+              <input
+                type="number"
+                name="nDni"
+                id="nDni"
+                placeholder=""
+                onChange={handlerInputChangeFromRegister}
+              ></input>
+            </div>
+            <div class={styles.inputGroup}>
+              <label for="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder=""
+                onChange={handlerInputChangeFromRegister}
+              ></input>
+            </div>
+            <div class={styles.inputGroup}>
+              <label for="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder=""
+                onChange={handlerInputChangeFromRegister}
+              ></input>
+            </div>
+            <button class={styles.sign}>Sign in</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
