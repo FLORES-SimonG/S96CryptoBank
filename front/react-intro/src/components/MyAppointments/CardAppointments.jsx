51e21b50190df7805bab04d6596c23585a7d1ec6
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-03-16 23:50:05 +0100

    Agregué condicional y estilos cuando se cancela un turno

--- front/react-intro/src/components/MyAppointments/CardAppointments.jsx
+++ front/react-intro/src/components/MyAppointments/CardAppointments.jsx
@@ -1,14 +1,21 @@
 import { BiCalendarCheck , BiCalendarX  } from 'react-icons/bi';
 import styles from './MyAppointmentsStyles.module.css';
+import handleAppointmentCancel from './MyAppointments.jsx';
 
-const CardAppointments = ({ turno: { date, time, user, status } }) => {
+const CardAppointments = ({ turno: { date, time, user, status } , handleAppointmentCancel,id}) => {
+
+const handleCancel = ()=>{
+  window.confirm("Are you sure you want to cancel this appointment?")?(handleAppointmentCancel(id), console.log("Cancelación cancelada")
+  ):(console.log("NO SE HA CANCELADO"));}
+
+
   return (
-    <div className={`${styles.card} ${styles.turn}`}>
+    <div className={status === 'active' ? `${styles.card} ${styles.turn}` : `${styles.card} ${styles.inactive}`}>
       <p>Date: {date}</p>
       <p>Time: {time}</p>
-      <p>User: {user}</p>
+      {/* <p>User: {user}</p> */}
       <p>
-        {status === 'active' ? <BiCalendarCheck  color="green" /> : <BiCalendarX  color="red" />}
+        {status === 'active' ? <BiCalendarCheck  color="green" onClick={handleCancel}/> : (<BiCalendarX  color="red"/>)}
       </p>
     </div>
   );
--- front/react-intro/src/components/MyAppointments/MyAppointments.jsx
+++ front/react-intro/src/components/MyAppointments/MyAppointments.jsx
@@ -10,32 +10,44 @@ import { setAppointments } from "../../redux/userSlice.js";
 
 const MyAppointments = () => {
   const dispatch = useDispatch();
+  const handleAppointmentCancel =(appointmentID)=>{
+    axios.put(`http://localhost:3000/appointments/cancel/${appointmentID}`)
+    .then(response=>response.data).then(data=>{
+      axios.get(`http://localhost:3000/users/${activeUserID}`).then(res=>dispatch(setAppointments(res.data.appointments))).catch(error=>(console.log(error.message),console.log(`Error al cancelar el turno con id ${appointmentID}, el error es: ${error?.response?.data?.message}`)))
+    })
+  }
+
 
   const activeUserID = useSelector(
     (state) => state.actualUser.userData.user.id
   );
+
   const activeUserAppointment = useSelector(
     (state) => state.actualUser.userAppointments
   ); //! ubicación de los appointments dentro del objeto
+
   console.log(activeUserID);
+
   useEffect(() => {
     axios
       .get(`http://localhost:3000/users/${activeUserID}`)
       .then((res) => dispatch(setAppointments(res.data.appointments)));
   }, []);
 
+
   return (
     <>
       <Navbar></Navbar>
       <div>
         <h2>My Appointments</h2>
-        <div className={styles.cards}></div>
+        {/* <div className={styles.cards}></div> */}
         <div className={styles.cards}>
-          {activeUserAppointment.map((appointment) => {
+          {activeUserAppointment.length?
+          activeUserAppointment.map((appointment) => {
             return (
-              <CardAppointments key={appointment.id} turno={appointment} />
+              <CardAppointments key={appointment.id} turno={appointment} id={appointment.id} handleAppointmentCancel={handleAppointmentCancel}/>
             );
-          })}
+          }):<h3>No appointments</h3>}
         </div>
       </div>
     </>
