732bbd3dc6eb9158af1b7f3d04249484d0380e54
Simón Gerardo Flores <simonflores_10@hotmail.com>
2024-03-16 22:05:05 +0100

    Actividad 4 completa

--- front/react-intro/src/components/Navbar/Navbar.jsx
+++ front/react-intro/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 import './NavbarStyle.module.css'; //!asi se incorpora!
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
 
 const Navbar = () => {
+    
+    const login = useSelector((state)=>state.actualUser.userData.login)
+
     return (
         <>
         <h1>S96 Bank</h1>
@@ -12,7 +17,7 @@ const Navbar = () => {
                 <li><Link to="/Home">Home</Link> </li>
                 <li><Link to="/login">Login</Link> </li>
                 <li><Link to="/register">Register</Link> </li>
-                <li><Link to="/myappointments">My Appointments</Link> </li>
+                {login && <li><Link to="/myappointments">My Appointments</Link> </li>}
                 
             </ul>
             
